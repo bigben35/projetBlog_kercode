2eaabc778e917e724ab74a5b459fc300a529a5a9
bigben35 <79405849+bigben35@users.noreply.github.com>
2022-03-03 14:33:52 +0100

    installation composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a875142b12e34e06051878932be69eeb09c817f4',
+        'name' => 'kercode/projet-blog_kercode',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'kercode/projet-blog_kercode' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a875142b12e34e06051878932be69eeb09c817f4',
+            'dev_requirement' => false,
+        ),
+    ),
+);
