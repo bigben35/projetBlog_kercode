331573cf43114200772d8c09fc341c1000253c17
bigben35 <79405849+bigben35@users.noreply.github.com>
2022-03-04 09:50:36 +0100

    composer installation

---
+++ app/Models/Manager.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ProjetBlogKercode\Models;
+
+use Exception;
+
+class Manager
+{
+    protected function dbConnect()  //protected car utiliser uniquement par Manager et les classes qui vont hériter de la classe Manager
+    {
+        try {
+            $bdd = new \PDO('mysql:host=localhost;dbname=blog_inmyhead;charset=utf8', 'root', '');
+            return $bdd;
+
+        } catch(Exception $e){
+            die('Erreur: ' . $e->getMessage());
+        }
+    }
+}
